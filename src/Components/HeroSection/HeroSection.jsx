3425053d2cdd561b5e147b55115c1307a8128b7b
Amrr-Maherr <amrr.maherr242gmail.com>
2025-07-17 00:09:23 +0300

    feat(hero-section): build dynamic HeroSection with API data and loading state
    - Fetched popular movies from TMDB using UseFetchData hook
    - Rendered background image for each slide using movie.backdrop_path
    - Centered movie title and custom "Watch now" button with full responsiveness
    - Displayed Loader component while data is being fetched
    - Improved accessibility with semantic tags and responsive text sizes

---
+++ src/Components/HeroSection/HeroSection.jsx
@@ -0,0 +1,35 @@
+import UseFetchData from "@/Hooks/useFetchData";
+import Slider from "../Slider/Slider";
+import { Button } from "../ui/button";
+import Loader from "../Loader/Loader";
+
+export default function HeroSection() {
+  const { loading, Data, error } = UseFetchData({ url: "/movie/popular" });
+
+  return (
+    loading ? <Loader/> : <Slider>
+      {Data?.map((movie) => (
+        <div
+          key={movie.id}
+          className="h-screen w-full bg-cover bg-center relative flex items-center justify-center text-white"
+          style={{
+            backgroundImage: `url(${process.env.NEXT_PUBLIC_API_imageBaseUrl}${movie.backdrop_path})`,
+          }}
+        >
+          {/* Dark overlay */}
+          <div className="absolute bg-black/50 inset-0 w-full h-full z-10" />
+
+          {/* Movie title + button */}
+          <div className="z-20 flex flex-col items-center gap-4 px-4 text-center">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-extrabold mb-6 text-center leading-tight">
+              {movie.original_title}
+            </h1>
+            <button className="bg-red-600 hover:bg-red-700 text-base sm:text-lg cursor-pointer md:text-xl lg:text-2xl font-semibold py-5 sm:py-4 md:py-3 px-6 sm:px-8 md:px-10 rounded-lg transition-all ease-in-out duration-300">
+              Watch now
+            </button>
+          </div>
+        </div>
+      ))}
+    </Slider>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,13 @@
 "use client";
 import Footer from "@/Components/Footer/Footer";
 import Header from "@/Components/Header/Header";
-import Loader from "@/Components/Loader/Loader";
-import UseFetchData from "@/Hooks/useFetchData";
-import Image from "next/image";
+import HeroSection from "@/Components/HeroSection/HeroSection";
 export default function Home() {
-  const { loading, Data, error } = UseFetchData({ url: "/movie/popular" });
-  console.log(Data);
-  console.log(loading);
   
   return (
     <>
       <Header />
+      <HeroSection/>
       <Footer />
     </>
   );
