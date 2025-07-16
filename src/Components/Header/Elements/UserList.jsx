01714a0a057fad887f61bd273fa40a24d8445189
Amrr-Maherr <amrr.maherr242gmail.com>
2025-07-16 23:04:52 +0300

    feat: add UserList component with tooltips for Search and Register
    - Created UserList component with two interactive buttons
    - Each button wrapped in a Tooltip using shadcn/ui components
    - Styled buttons with red background and hover effects
    - Tooltips provide user guidance for actions requiring sign-up

--- src/Components/Header/Elements/UserList.jsx
+++ src/Components/Header/Elements/UserList.jsx
@@ -1,12 +1,46 @@
+import { Button } from "@/Components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "../../../Components/ui/tooltip";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "../../../Components/ui/navigation-menu";
 export default function UserList() {
     return (
       <>
         <div className="flex items-center justify-center gap-4">
           <div>
-            <p>test</p>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button className="bg-red-600 hover:bg-red-800 cursor-pointer">
+                  Search
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>You need to sign up to use search</p>
+              </TooltipContent>
+            </Tooltip>
           </div>
           <div>
-            <p>test</p>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button className="bg-red-600 hover:bg-red-800 cursor-pointer">
+                  Register
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Sign up to browse</p>
+              </TooltipContent>
+            </Tooltip>
           </div>
         </div>
       </>
