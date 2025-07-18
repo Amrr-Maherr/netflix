6d6d37bf6bdced75c80c96612f9d2bdfe9483928
Amrr-Maherr <amrr.maherr242gmail.com>
2025-07-18 10:15:49 +0300

    update navbar style to  fixed z-20 w-full

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ import UserList from "./Elements/UserList.jsx"
 export default function Header() {
     return (
       <>
-        <header className="p-4 bg-black/50 text-white">
+        <header className="p-4 bg-black/50 text-white fixed z-20 w-full">
           <nav className="flex items-center justify-between">
            <Logo/>
             <HeaderLinks />
