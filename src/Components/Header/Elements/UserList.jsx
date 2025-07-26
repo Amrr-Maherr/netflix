28268bbdf22d736d51b9a692a09860305ef47a81
Amrr-Maherr <amrr.maherr242gmail.com>
2025-07-26 15:12:13 +0300

    add useEffect to ensure user data is fetched on component mount

--- src/Components/Header/Elements/UserList.jsx
+++ src/Components/Header/Elements/UserList.jsx
@@ -1,71 +1,69 @@
+"use client";
+
 import { Button } from "@/Components/ui/button";
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "../../../Components/ui/tooltip";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "../../../Components/ui/navigation-menu";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function UserList({ mobile }) {
-  const storedUser = JSON.parse(localStorage.getItem("userInfo"));
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+      const storedUser = JSON.parse(localStorage.getItem("userInfo") || "null");
+      setUsers(storedUser);
+  }, []);
+
   return (
-    <>
-      <div
-        className={`flex items-center justify-center gap-4 ${
-          mobile ? "mt-auto h-full mb-[30px]" : ""
-        }`}
-      >
+    <div
+      className={`flex items-center justify-center gap-4 ${
+        mobile ? "mt-auto h-full mb-[30px]" : ""
+      }`}
+    >
+      <div>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href="/Search" passHref>
+              <Button
+                variant="default"
+                className="bg-red-500 hover:bg-red-700 cursor-pointer"
+              >
+                Search
+              </Button>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            {users ? (
+              <p>You can use search</p>
+            ) : (
+              <p>You need to sign up to use search</p>
+            )}
+          </TooltipContent>
+        </Tooltip>
+      </div>
+
+      {!users && (
         <div>
           <Tooltip>
             <TooltipTrigger asChild>
-              <Link href="/Search" passHref>
+              <Link href="/Register" passHref>
                 <Button
                   variant="default"
                   className="bg-red-500 hover:bg-red-700 cursor-pointer"
                 >
-                  Search
+                  Register
                 </Button>
               </Link>
             </TooltipTrigger>
             <TooltipContent>
-              {storedUser ? (
-                <p>You can use search</p>
-              ) : (
-                <p>You need to sign up to use search</p>
-              )}
+              <p>Sign up to browse</p>
             </TooltipContent>
           </Tooltip>
         </div>
-        {!storedUser && (
-          <div>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link href="/Register" passHref>
-                  <Button
-                    variant="default"
-                    className="bg-red-500 hover:bg-red-700 cursor-pointer"
-                  >
-                    Register
-                  </Button>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>Sign up to browse</p>
-              </TooltipContent>
-            </Tooltip>
-          </div>
-        )}
-      </div>
-    </>
+      )}
+    </div>
   );
 }
