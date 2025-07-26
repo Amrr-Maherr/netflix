6954cf4234eadc0393c8fc1fdb28f8207b8973d4
Amrr-Maherr <amrr.maherr242gmail.com>
2025-07-26 14:59:27 +0300

    update SideBar component styles and ensure it is included in relevant pages

--- src/Components/Header/SideBar/Eelemnts/SideBar.jsx
+++ src/Components/Header/SideBar/Eelemnts/SideBar.jsx
@@ -27,7 +27,7 @@ export default function SideBar({ mobile }) {
           animate={{ x: 0 }}
           exit={{ x: -1000 }}
           transition={{ duration: 0.6, ease: "easeOut" }}
-          className="p-4 bg-black/50 text-white fixed z-20 h-full w-[250px] block md:hidden"
+          className="p-4 bg-black text-white fixed z-20 h-full w-[250px] block md:hidden"
         >
           <CircleX
             className="absolute top-2 right-2 cursor-pointer"
